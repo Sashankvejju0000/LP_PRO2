bfda4dc907c0122fcdccf24f0076631318aa37d5
Sashankvejju0000 <spikysashank@gmail.com>
2026-05-26 21:40:30 +0530

    FIX

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/LP_PRO2/",
+  base: procress.env.VITE_BASE_PATH || "/LP_PRO2",
   plugins: [react()],
 })
